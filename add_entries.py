a3842eb9577237744b1c91e5b915e37908d8e27b
MeGaNeKo <evictory90@gmail.com>
2022-07-21 12:32:34 +0700

    refactor: change the parsing order from parsing filename first to edit the filename first

--- add_entries.py
+++ add_entries.py
@@ -7,32 +7,34 @@ with open('./anime-fansub-relation.json', "r+", encoding="utf-8") as input_json:
     anime_fansub_relation = json.load(input_json)
 
 
-def parse(file_name):
-    anime = anitopy.parse(file_name)
-    # replace any non-alphanumeric character with space and convert to lower case
-    anime_name = re.sub(r'[^A-Za-z\d]+', ' ', anime["anime_title"]).lower()
+def parse(filename):
+    filename = re.sub(r'(\d+)v(\d+)', r'\1 v\2 ', filename)
     """
-        ReplaceString(str, 0, L"&", L"and", true, true);
-        ReplaceString(str, 0, L"the animation", L"", true, true);
-        ReplaceString(str, 0, L"the", L"", true, true);
-        ReplaceString(str, 0, L"episode", L"", true, true);
-        ReplaceString(str, 0, L"oad", L"ova", true, true);
-        ReplaceString(str, 0, L"oav", L"ova", true, true);
-        ReplaceString(str, 0, L"specials", L"sp", true, true);
-        ReplaceString(str, 0, L"special", L"sp", true, true);
-        ReplaceString(str, 0, L"(tv)", L"", true, true);
-        """
+            ReplaceString(str, 0, L"&", L"and", true, true);
+            ReplaceString(str, 0, L"the animation", L"", true, true);
+            ReplaceString(str, 0, L"the", L"", true, true);
+            ReplaceString(str, 0, L"episode", L"", true, true);
+            ReplaceString(str, 0, L"oad", L"ova", true, true);
+            ReplaceString(str, 0, L"oav", L"ova", true, true);
+            ReplaceString(str, 0, L"specials", L"sp", true, true);
+            ReplaceString(str, 0, L"special", L"sp", true, true);
+            ReplaceString(str, 0, L"(tv)", L"", true, true);
+    """
     # replace the anime name where found a word with the another word only if whole word is found
-    anime_name = re.sub(r'\b&\b', 'and', anime_name, flags=re.IGNORECASE)
-    # anime_name = re.sub(r'\bthe animation\b', '', anime_name, flags=re.IGNORECASE)
-    # anime_name = re.sub(r'\bthe\b', '', anime_name, flags=re.IGNORECASE)
-    # anime_name = re.sub(r'\bepisode\b', '', anime_name, flags=re.IGNORECASE)
-    anime_name = re.sub(r'\boad\b', 'ova', anime_name, flags=re.IGNORECASE)
-    anime_name = re.sub(r'\boav\b', 'ova', anime_name, flags=re.IGNORECASE)
-    anime_name = re.sub(r'\bspecials\b', 'sp', anime_name, flags=re.IGNORECASE)
-    anime_name = re.sub(r'\bspecial\b', 'sp', anime_name, flags=re.IGNORECASE)
+    filename = re.sub(r'\b&\b', 'and', filename, flags=re.IGNORECASE)
+    filename = re.sub(r'\boads?\b', 'ova', filename, flags=re.IGNORECASE)
+    filename = re.sub(r'\boavs?\b', 'ova', filename, flags=re.IGNORECASE)
+    # filename = re.sub(r'\bthe animation\b', '', filename, flags=re.IGNORECASE)
+    # filename = re.sub(r'\bthe\b', '', filename, flags=re.IGNORECASE)
+    # filename = re.sub(r'\bepisode\b', '', filename, flags=re.IGNORECASE)
+    filename = re.sub(r'\bspecials?\b', 'special', filename, flags=re.IGNORECASE)
+    anime = anitopy.parse(filename)
+    # remove everything in bracket from the title
+    anime_name = re.sub(r"[(\[{].*?[)\]}]|[-:]", ' ', anime['anime_title'])
+    # replace any non-alphanumeric character with space and convert to lower case
+    anime_name = re.sub(r'[^A-Za-z\d]+', ' ', anime_name).lower()
 
-    anime["anime_title"] = anime_name
+    anime["anime_title"] = anime_name.strip()
     return anime
 
 
@@ -50,30 +52,41 @@ def main() -> None:
     if anime_relation:
         # if we have the anime season in the anime relation
         # the anime season should be a string of a number without leading zero and can't be negative
+        message = anime['anime_title']
         print("Found in the anime in database")
         if anime_season:
+            message += f" Season {int(anime_season)}"
             anime_relation = anime_relation.get(str(int(anime_season)), None)
 
         # this can be change on code above, if we have the anime season in the anime relation
         # if we don't have the anime season in the anime relation, then we want to ask the user to enter the anime id
+        # optional
+        if anime.get("anime_type", None):
+            message += f" type \"{anime['anime_type']}\""
+            anime_relation = anime_relation.get(anime.get("anime_type", "").lower(), anime_relation)
+
+        if anime.get("anime_year", 0):
+            message += f" {anime['anime_year']}"
+            anime_relation = anime_relation.get(anime.get("anime_year", "").lower(), anime_relation)
+
         if anime_relation:
             # if the anime fansub in the anime relation
-            anime_id = anime_relation.get(anime.get('release_group'), None)
+            anime_id = anime_relation.get(anime.get('release_group').lower(), None)
             # else we return the default value
             if anime_id is None:
                 anime_id = anime_relation.get("anilist", None)
-                print(f"Anime id are {anime_id}")
+                print(f"Anime id for \"{message}\" are {anime_id}")
             else:
-                print(f"Anime with fansub from {anime.get('release_group')} are {anime_id}")
+                print(f"{message} from {anime.get('release_group')} are {anime_id}")
             return
         else:
-            print(f"No Season {int(anime_season)} in database")
+            print(f"{message} season {int(anime_season)} not in database")
 
     # we didn't found in database
     # add the anime to the database
     if anime_id is None:
         print(f"{anime['anime_title']} not found")
-
+        message = anime['anime_title']
         # get the anime from anime fansub relation
         # create a new one if not exist
         try:
@@ -84,28 +97,27 @@ def main() -> None:
         # ask user the correct anilist id of the anime
         anime_id = int(input("Please input the anime anilist id: "))
 
-        # ask user if the rule apply to the season or the base anime
         if anime.get("anime_season", None):
-            season_only = input(f"Apply the rule for Season {int(anime['anime_season'])} only? (y/n): ")
-            # if yes, then go to the season level of that anime
-            # create a new one if not exist
-            if season_only.lower() == "y":
-                try:
-                    this_anime = this_anime[str(int(anime_season))]
-                except KeyError:
-                    this_anime = this_anime[str(int(anime_season))] = {}
+            message += f" Season {anime['anime_season']}"
+            print(f"This rule only affect Season {int(anime['anime_season'])}")
+            try:
+                this_anime = this_anime[str(int(anime_season))]
+            except KeyError:
+                this_anime[str(int(anime_season))] = {}
+                this_anime = this_anime[str(int(anime_season))]
 
         # ask user if the rule apply for global or this fansub only
         if anime.get("release_group", None):
             print("Fansub detected as " + anime["release_group"])
             fansub_only = input("Apply the rule for this fansub only? (y/n): ")
             if fansub_only.lower() == "y":
+                message += f" from \"{anime['release_group']}\""
                 this_anime[anime["release_group"]] = anime_id
             else:
                 this_anime["anilist"] = anime_id
         else:
             this_anime["anilist"] = anime_id
-        print(f"{anime['anime_title']} has set to id: {anime_id}")
+        print(f"{message} has set to id: {anime_id}")
 
 
 if __name__ == "__main__":
--- readme.md
+++ readme.md
@@ -1,49 +1,74 @@
 # Anime Fansub Relations
 
-This repository includes anime fansub season relation data. It is used to detect the fansub seasoning number to the correct anime.<br>
+This repository includes anime fansub season relation data. It is used to detect the fansub seasoning number to the
+correct anime.<br>
 
 Example:
-- `black clover` released from judas's fansub has `4 season` for `170 episode` whereas the anime has only `1 season`. `([Judas] Black Clover - S02E17 (068).mkv)`
-- `to love ru` has `4 season` where the `season 2` has difference name with `season 1`, and `season 3` has difference name with `season 2`. `([Judas] To Love-Ru - S04E08.mkv)`
 
+- `black clover` released from judas's fansub has `4 season` for `170 episode` whereas the anime has only `1 season`
+  . `([Judas] Black Clover - S02E17 (068).mkv)`
+- `to love ru` has `4 season` where the `season 2` has difference name with `season 1`, and `season 3` has difference
+  name with `season 2`. `([Judas] To Love-Ru - S04E08.mkv)`
 
 ## Rule syntax
-### json resource representation
+
+## Nodes.
+
+Since the difference season can have same title, So we have a node for that title. <br>
+All node that mark as `required` mean if it in the `anime filename` but not in the `fansub relation`, then it
+returns `not detected`. <br>
+In the case the `optional` node doesn't exist, then continue from the last entered node. <br>
+A node can have another sub node as long the deepest node have `fansub_name` or `anilist` id. <br>
+
+A node structure as follows:
+
 ```
-{
-  "title": { // required, alphanumeric, lowercase
-    "season": { // optional, number
-      "fansub_name": <anilist_id: number>, // optional
-      "anilist": <anilist_id: number>, // default id if no fansub specified
-    },
-    "fansub_name": <anilist_id: number>, // optional
-    "anilist": <anilist_id: number>, // default id if no fansub specified
-  }
-}
+Detected title // required
+    Season // required
+        Anime Type // optional
+            Anime Year // optional
+                // at least one of the following must be present:
+                fansub_name, anilist // required
 ```
-**All id are using anilist** `https://anilist.co/anime/{id}/{title}`
+
+**All id are using anilist** `https://anilist.co/anime/{id}/{title}` <br>
+**All key should be `lowercase, alphanumeric, without any leading or trailing spaces` string**
+
+## Naming Rule
+
+All anime detection should be:
+
+- If there is any `ova, oad, oav` should be replaced with `ova`. (No plural like `ovas, oads, oavs`) <br>
+- Remove anything inside bracket, square bracket, parenthesis, etc. <br>
+- `{title}` should be in lowercase, alphanumeric, without any leading or trailing spaces. <br>
 
 ## Example
+
 ```json
 {
-    "to love ru": {
-        "1": {
-            "anilist": 30671
-        },
-        "2": {
-            "anilist": 9181
-        },
-        "3": {
-            "anilist": 13663
-        },
-        "4": {
-            "judas": 21853,
-            "anilist": 21853
-        },
-        "anilist": 30671
+  "86 eighty six": {
+    "2021": {
+      "judas": 131586
     }
+  },
+  "to love ru": {
+    "1": {
+      "anilist": 30671
+    },
+    "2": {
+      "anilist": 9181
+    },
+    "3": {
+      "anilist": 13663
+    },
+    "4": {
+      "anilist": 21853
+    },
+    "anilist": 30671
+  }
 }
 ```
 
 ## License
+
 This repository is in the public domain.
